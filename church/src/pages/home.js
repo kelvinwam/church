cf1e6b28a123ac25f743b9e3f7892c54a0ec21b0
kelvinwam <wambuakelvin14@gmail.com>
2024-10-19 21:54:11 +0300

    added the pastors section

--- church/src/pages/home.js
+++ church/src/pages/home.js
@@ -1,6 +1,4 @@
 import React from 'react';
-// import { Button, Row, Col } from 'react-bootstrap';
-// import Form from 'react-bootstrap/Form';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -8,6 +6,16 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import CallIcon from '@mui/icons-material/Call';
+import SmsIcon from '@mui/icons-material/Sms';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+import Icon from '@mui/material/Icon';
+import Avatar from '@mui/joy/Avatar';
+import Chip from '@mui/joy/Chip';
+import Box from '@mui/joy/Box';
+import Card from '@mui/joy/Card';
+import CardContent from '@mui/joy/CardContent';
+import Typography from '@mui/joy/Typography';
 
 function createData(service, time) {
     return { service, time };
@@ -29,22 +37,6 @@ const Home = () => {
                 <div className="banner-text">
                     <h1>GALATIANS 6:9</h1>
                     <p>Let us not become weary in doing good, for at the proper time we will reap a harvest if we do not give up.</p>
-                    {/* <h3>
-                        Subscribe to our Newsletter
-                    </h3>
-                    <Form inline>
-                        <Row>
-                            <Col xs='center'>
-                                <input
-                                    type="email"
-                                    placeholder="Email address"
-                                    className=" mr-sm-2 " required />
-                            </Col>
-                            <br /><br />
-                            <Col xs='center'>
-                                <Button type="submit">Subscribe</Button></Col>
-                        </Row>
-                    </Form> */}
                 </div>
             </div>
             {/* services timetable */}
@@ -80,8 +72,175 @@ const Home = () => {
                     <a href="https://www.gps.ie/">gps devices</a>
                 </iframe>
             </div>
+            {/* pastors' section */}
             <div className='pastors'>
-                <h1>Pastors</h1>
+                <h1 className='h1'>Our Pastors</h1>
+                <div className="pastors-cards">
+                    <Card sx={{ width: 320, maxWidth: '100%', boxShadow: 'lg' }}>
+                        <CardContent sx={{ alignItems: 'center', textAlign: 'center' }}>
+                            <Avatar src="/static/images/avatar/1.jpg" sx={{ '--Avatar-size': '4rem' }} />
+                            <Chip
+                                size="sm"
+                                variant="soft"
+                                color="primary"
+                                sx={{
+                                    mt: -1,
+                                    mb: 1,
+                                    border: '3px solid',
+                                    borderColor: 'background.surface',
+                                }}
+                            >
+                                Pastor in Charge
+                            </Chip>
+                            <Typography level="title-lg">Rev. John Musili</Typography>
+                            <Typography level="body-sm" sx={{ maxWidth: '24ch' }}>
+                                Hello, this is my bio and I am a PRO member of MUI. I am a developer and I love to code.
+                            </Typography>
+                            <Box
+                                sx={{
+                                    display: 'flex',
+                                    gap: 2,
+                                    mt: 2,
+                                    '& > button': { borderRadius: '2rem' },
+                                }}
+                            >
+                                <a href='tel:+254780966228'><Icon title='Call' sx={{
+                                    height: 50, cursor: 'pointer'
+                                }}><CallIcon href='tel:+254780966228' /></Icon></a>
+                                <a href='https://wa.me/+254780966228'><Icon title='Sms' sx={{
+                                    height: 50, cursor: 'pointer'
+                                }}><SmsIcon /></Icon></a>
+                                <a href='https://wa.me/+254780966228'><Icon title='Whatsapp' sx={{
+                                    height: 50, cursor: 'pointer'
+                                }}><WhatsAppIcon /></Icon></a>
+                            </Box>
+                        </CardContent>
+                    </Card>
+                    <Card sx={{ width: 320, maxWidth: '100%', boxShadow: 'lg' }}>
+                        <CardContent sx={{ alignItems: 'center', textAlign: 'center' }}>
+                            <Avatar src="/static/images/avatar/1.jpg" sx={{ '--Avatar-size': '4rem' }} />
+                            <Chip
+                                size="sm"
+                                variant="soft"
+                                color="primary"
+                                sx={{
+                                    mt: -1,
+                                    mb: 1,
+                                    border: '3px solid',
+                                    borderColor: 'background.surface',
+                                }}
+                            >
+                                Reverend
+                            </Chip>
+                            <Typography level="title-lg">Mr. Katonny</Typography>
+                            <Typography level="body-sm" sx={{ maxWidth: '24ch' }}>
+                                Hello, this is my bio and I am a PRO member of MUI. I am a developer and I love to code.
+                            </Typography>
+                            <Box
+                                sx={{
+                                    display: 'flex',
+                                    gap: 2,
+                                    mt: 2,
+                                    '& > button': { borderRadius: '2rem' },
+                                }}
+                            >
+                                <a href='tel:+254780966228'><Icon title='Call' sx={{
+                                    height: 50, cursor: 'pointer'
+                                }}><CallIcon href='tel:+254780966228' /></Icon></a>
+                                <a href='https://wa.me/+254780966228'><Icon title='Sms' sx={{
+                                    height: 50, cursor: 'pointer'
+                                }}><SmsIcon /></Icon></a>
+                                <a href='https://wa.me/+254780966228'><Icon title='Whatsapp' sx={{
+                                    height: 50, cursor: 'pointer'
+                                }}><WhatsAppIcon /></Icon></a>
+                            </Box>
+                        </CardContent>
+                    </Card>
+                    <Card sx={{ width: 320, maxWidth: '100%', boxShadow: 'lg' }}>
+                        <CardContent sx={{ alignItems: 'center', textAlign: 'center' }}>
+                            <Avatar src="/static/images/avatar/1.jpg" sx={{ '--Avatar-size': '4rem' }} />
+                            <Chip
+                                size="sm"
+                                variant="soft"
+                                color="primary"
+                                sx={{
+                                    mt: -1,
+                                    mb: 1,
+                                    border: '3px solid',
+                                    borderColor: 'background.surface',
+                                }}
+                            >
+                                Pastor
+                            </Chip>
+                            <Typography level="title-lg">Mrs. Lumumba</Typography>
+                            <Typography level="body-sm" sx={{ maxWidth: '24ch' }}>
+                                Hello, this is my bio and I am a PRO member of MUI. I am a developer and I love to code.
+                            </Typography>
+                            <Box
+                                sx={{
+                                    display: 'flex',
+                                    gap: 2,
+                                    mt: 2,
+                                    '& > button': { borderRadius: '2rem' },
+                                }}
+                            >
+                                <a href='tel:+254780966228'><Icon title='Call' sx={{
+                                    height: 50, cursor: 'pointer'
+                                }}><CallIcon href='tel:+254780966228' /></Icon></a>
+                                <a href='https://wa.me/+254780966228'><Icon title='Sms' sx={{
+                                    height: 50, cursor: 'pointer'
+                                }}><SmsIcon /></Icon></a>
+                                <a href='https://wa.me/+254780966228'><Icon title='Whatsapp' sx={{
+                                    height: 50, cursor: 'pointer'
+                                }}><WhatsAppIcon /></Icon></a>
+                            </Box>
+                        </CardContent>
+                    </Card>
+                    <Card sx={{ width: 320, maxWidth: '100%', boxShadow: 'lg' }}>
+                        <CardContent sx={{ alignItems: 'center', textAlign: 'center' }}>
+                            <Avatar src="/static/images/avatar/1.jpg" sx={{ '--Avatar-size': '4rem' }} />
+                            <Chip
+                                size="sm"
+                                variant="soft"
+                                color="primary"
+                                sx={{
+                                    mt: -1,
+                                    mb: 1,
+                                    border: '3px solid',
+                                    borderColor: 'background.surface',
+                                }}
+                            >
+                                Pastor
+                            </Chip>
+                            <Typography level="title-lg">Mrs. Betty Mbithi</Typography>
+                            <Typography level="body-sm" sx={{ maxWidth: '24ch' }}>
+                                Hello, this is my bio and I am a PRO member of MUI. I am a developer and I love to code.
+                            </Typography>
+                            <Box
+                                sx={{
+                                    display: 'flex',
+                                    gap: 2,
+                                    mt: 2,
+                                    '& > button': { borderRadius: '2rem' },
+                                }}
+                            >
+                                <a href='tel:+254780966228'><Icon title='Call' sx={{
+                                    height: 50, cursor: 'pointer'
+                                }}><CallIcon href='tel:+254780966228' /></Icon></a>
+                                <a href='https://wa.me/+254780966228'><Icon title='Sms' sx={{
+                                    height: 50, cursor: 'pointer'
+                                }}><SmsIcon /></Icon></a>
+                                <a href='https://wa.me/+254780966228'><Icon title='Whatsapp' sx={{
+                                    height: 50, cursor: 'pointer'
+                                }}><WhatsAppIcon /></Icon></a>
+                            </Box>
+                        </CardContent>
+                    </Card>
+                </div>
+            </div>
+            {/* groups section */}
+            <div className='groups'>
+                
             </div>
         </div>
     );
